0beffb6ddf1980095665e8e1d8a7f41127fa924d
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-02-19 11:46:23 -0500

    weight

--- app.py
+++ app.py
@@ -13,11 +13,19 @@
 # print('Pytho' in course and "beginners" in course)
 # print(not 'Python' in course)
 
-temperature = 22
+# temperature = 22
 
-if(temperature > 30):
-    print("It's a hot day")
-elif(temperature > 20):
-    print("It's a nice day")
-else:
-    print("It's not a hot day")
+# if(temperature > 30):
+#     print("It's a hot day")
+# elif(temperature > 20):
+#     print("It's a nice day")
+# else:
+#     print("It's not a hot day")
+
+weight = float(input("Enter your weight: "))
+unit = input("(L)bs or (K)g: ")
+
+if unit.upper() == "L":
+    print(f"You are {weight * 0.45} kilos")
+if unit.upper() == "K":
+    print(f"You are {weight / 0.45} pounds")
