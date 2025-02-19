e6fb62620f8f55f3474f2b24493b65657be7d0e9
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-02-19 11:41:01 -0500

    if

--- app.py
+++ app.py
@@ -4,11 +4,20 @@
 
 # print("Sum of two numbers is: ", first + second)
 
-course = "Python for beginners"
+# course = "Python for beginners"
 
-print(course.upper())
-print(course.lower())
-print(course.replace("r", "R"))
-print('Pytho' in course)
-print('Pytho' in course and "beginners" in course)
-print(not 'Python' in course)
+# print(course.upper())
+# print(course.lower())
+# print(course.replace("r", "R"))
+# print('Pytho' in course)
+# print('Pytho' in course and "beginners" in course)
+# print(not 'Python' in course)
+
+temperature = 22
+
+if(temperature > 30):
+    print("It's a hot day")
+elif(temperature > 20):
+    print("It's a nice day")
+else:
+    print("It's not a hot day")
