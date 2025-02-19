b49753c0ae86c6c59834e11658f0682215367a1b
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-02-19 11:19:14 -0500

    strings

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 
-first = float(input("Enter first number: "))
-second = float(input("Enter second number: "))
+# first = float(input("Enter first number: "))
+# second = float(input("Enter second number: "))
 
-print("Sum of two numbers is: ", first + second)
+# print("Sum of two numbers is: ", first + second)
+
+course = "Python for beginners"
+
+print(course.upper())
+print(course.lower())
+print(course.replace("r", "R"))
